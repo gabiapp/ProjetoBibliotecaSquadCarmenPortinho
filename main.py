027441cc5824300e5f20498c7249d61c9793cfc4
Gabriela A. Z. Nunez <gabrielaap.nunez@gmail.com>
2024-02-04 16:27:46 -0300

    Atualização 0402 - Adicionando classe de Exemplares

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 
-Class Usuario:
+class Usuario:
   def __init__(self, nome, telefone, nacionalidade):
     self.nome = nome
     self.telefone = telefone
@@ -8,3 +8,13 @@ Class Usuario:
 
   #criar usuário
   #usuario1 = Usuario("Mariana", "4999999999", "Brasileira")
+    
+class Exemplar:
+  def __init__(self, livro)
+    self.livro = livro
+    self.status = "Disponível"
+
+    #adicionar exemplares aos livros
+    #exemplar01 = Exemplar("livro01")
+    
+    #livro01.adcionar_exemplar(exemplar01)
