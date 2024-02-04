f8e480c554717a882999c0b6de4451c8215c8254
danisoaresl <daniele.soaresl@gmail.com>
2024-02-04 16:07:49 -0300

    atualizacao0402

---
+++ main.py
@@ -0,0 +1,10 @@
+from datetime import datetime
+
+Class Usuario:
+  def __init__(self, nome, telefone, nacionalidade):
+    self.nome = nome
+    self.telefone = telefone
+    self.nacionalidade = nacionalidade
+
+  #criar usuário
+  #usuario1 = Usuario("Mariana", "4999999999", "Brasileira")
