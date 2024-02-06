f2d9d38c8e3d16a46de9f7b3fbfdc8f18d98c814
Your Name <you@example.com>
2024-02-05 22:54:09 -0300

    atualizando projeto

--- biblioteca.py
+++ biblioteca.py
@@ -4,22 +4,11 @@ conexao = sqlite3.connect('biblioteca.db')
 type(conexao)
 cursor = conexao.cursor()
 
-cursor.execute('CREATE TABLE livro( idlivro INTEGER PRIMARY KEY , titulo VARCHAR(100), editora VARCHAR(100), max_renovacao INTEGER)')
+cursor.execute('CREATE TABLE livro( idlivro INTEGER NOT NULL, titulo VARCHAR (100) NOT NULL, descricao VARCHAR (100) NOT NULL, idexemplar INTEGER NOT NULL, id_autor INTEGER NOT NULL, id_genero INTEGER NOT NULL, status VARCHAR (10) NOT NULL, editora VARCHAR(100) NOT NULL, PRIMARY KEY (idlivro)')
 
-cursor.execute('CREATE TABLE exemplar( idexemplar INTEGER PRIMARY KEY, FOREIGN KEY (idlivro) REFERENCES livro(idlivro), status VARCHAR(10))')
+cursor.execute('CREATE TABLE usuario( idusuario INTEGER NOT NULL, nome VARCHAR (100) NOT NULL, telefone VARCHAR (12) NOT NULL, nacionalidade VARCHAR(20) NOT NULL, PRIMARY KEY (idusuario))')
 
-cursor.execute('CREATE TABLE usuario( idusuario INTEGER PRIMARY KEY, telefone VARCHAR (12), nacionalidade VARCHAR(20))')
-
-cursor.execute('CREATE TABLE autor_livro( FOREIGN KEY (idlivro) REFERENCES livro(idlivro), id_autor INTEGER PRIMARY KEY )')
-
-cursor.execute('CREATE TABLE genero_livro( FOREIGN KEY (idlivro) REFERENCES livro(idlivro), id_genero INTEGER PRIMARY KEY)')
-
-cursor.execute('CREATE TABLE historico ( id_historico INTEGER PRIMARY KEY, FOREIGN KEY (idusuario) REFERENCES usuario(idusuario), FOREIGN KEY (idexemplar) REFERENCES exemplar(idexemplar), data_emprestimo DATE NOT NULL, data_devolucao DATE NOT NULL, quantidade_renovacao INTERGER)')
-
-cursor.execute('CREATE TABLE usuario( FOREIGN KEY (id_autor) REFERENCES autor_livro(id_autor), nome VARCHAR (100))')
-
-cursor.execute('CREATE TABLE genero( OREIGN KEY (id_genero) REFERENCES genero_livro(id_genero), descricao VARCHAR (100))')
-
+cursor.execute('CREATE TABLE historico ( id_historico INTEGER NOT NULL, data_emprestimo DATE NOT NULL, data_devolucao DATE NOT NULL, quantidade_renovacao INTERGER NOT NULL, PRIMARY KEY id_historico, FOREIGN KEY(idusuario) REFERENCER usuario(idusuario), FOREIGN KEY(idexemplar) REFERENCER livro(idexemplar))')
 
 conexao.commit() 
 conexao.close
